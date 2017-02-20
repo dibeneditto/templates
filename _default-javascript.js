936fb4142553eecd917becc98ff636c4cf557548
Lukas W. DiBeneditto <lukas@dibeneditto.com>
2017-02-19 23:25:35 -0500

    Create _default-javascript.js

---
+++ _default-javascript.js
@@ -0,0 +1,12 @@
+/**
+ * Type description here.
+ * 
+ * @author Lukas W. DiBeneditto <lukas@dibeneditto.com>
+ * @see {@link http://example.com Title}
+ * @version 1.0.0
+ */
+function main() {
+  return "Main function";
+}
+
+main();
